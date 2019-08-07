5b03f094fa7623b57dcc0410916682d459b4237e
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-08-06 21:03:26 -0600

    Routes setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import ListsOverview from "components/ListsOverview/ListsOverview";
+import AppContext from "components/AppContext/AppContext";
+import Layout from "./components/Layout/Layout";
+import "styles/index.scss";
 
-const App = () =>{
+const App = () => {
   return (
-    <div>
-      <h1>Hello Parcel</h1>
-    </div>
-  )
+    <AppContext>
+      <Layout>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ListsOverview} />
+          </Switch>
+        </BrowserRouter>
+      </Layout>
+    </AppContext>
+  );
 };
 render(<App />, document.getElementById("app"));
